fd3deada1f9b54e6fe8295ea09f44f7c2dd31116
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-06-22 11:19:33 +0500

    added websocket method handler

--- Services/SocketsHandler.cs
+++ Services/SocketsHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.WebSockets;
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
@@ -6,9 +8,17 @@ namespace WebSockets.Services
 {
     public class SocketsHandler : ISocketsHandler
     {
-        public Task Handle(HttpContext context, WebSocket webSocket)
+        public async Task Handle(HttpContext context, WebSocket webSocket)
         {
-            throw new System.NotImplementedException();
+            var buffer = new byte[1024 * 4];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType,
+                    result.EndOfMessage, CancellationToken.None);
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
